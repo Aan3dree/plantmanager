9bca8bebb93b331aab915053dedb5508e92e0784
Andre Souza <an3dree@gmail.com>
2021-04-20 10:24:32 -0300

    aula 1

--- src/views/Welcome.tsx
+++ src/views/Welcome.tsx
@@ -1,28 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {SafeAreaView, View, Text, Image, TouchableOpacity, StyleSheet} from 'react-native';
 
 import wateringImg from '../assets/watering.png';
+import { Botao } from '../components/Botao';
+import colors from '../styles/colors';
 
 export function Welcome()
 {
+    const [visible, setVisible] = useState(false);
+
+    function handleVisibily(){
+        setVisible(true);
+    }
     return(
         <SafeAreaView style={styles.container}>
-            <Text>
-            Gerencie suas plantas de forma fácil
+            <Text style={styles.title}>
+            Gerencie {'\n'} 
+            suas plantas {'\n'} 
+            de forma fácil
             </Text>
 
-            <Image source={wateringImg} />
-
-            <Text>
+            {
+                visible &&
+                <Image source={wateringImg} style={styles.image} />
+            }
+            <Text style={styles.subtitle}>
                 Não esqueça mais de regar suas plantas.
                 Nós cuidamos de lembrar você sempre que precisar
             </Text>
 
-            <TouchableOpacity>
-                <Text>
-                    ':D'
-                </Text>
-            </TouchableOpacity>
+            <Botao title="=" onPress={handleVisibily}/>
 
         </SafeAreaView>
     )
@@ -33,5 +40,24 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'space-between'
-    }
+    },
+    title:{
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#52665A',
+        marginTop: 38
+    },
+    subtitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 20,
+        color: '#52665A'
+    },
+    
+    image:{
+        width: 292,
+        height: 284
+    },
+    
 });
